70f0f2528556edd1fc0aa51ef7d9e72c270455e6
Anastasia Vyugina <75266068+avyuga@users.noreply.github.com>
2021-05-25 09:35:53 +0300

    3.1

---
+++ Lab7/code_cycle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+import time
+
+
+def saturate(x, left, right):
+    if x > right: x = right
+    if x < left: x = left
+    return x
+
+
+sound = Sound()
+sound.set_volume(100)
+sound.beep()
+
+# первая координата - motorA
+# вторая координата - motorB
+# третья координата - motorC
+Q = [[90, 45, 45], [90, 50, 60], [70, 50, 60], [90, 40, 40], [90, 80, 80]]
+
+# значение коэффициентов в градусных мерах
+k_p = [0.3, 0.3, 0.1]
+k_i = [0.25 / 60, 0.25 / 60, 0]
+k_d = [1 / 60, 1 / 60, 0]
+
+inaccuracy = 5  # погрешность в градусах
+U_max = 6.97
+
+motorA = LargeMotor('outA')
+motorB = LargeMotor('outB')
+motorC = MediumMotor('outC')
+
+motorA.position = 0
+motorB.position = 0
+motorC.position = 0
+
+motors_set = [motorA, motorB, motorC]
+
+for i in range(5):
+    q0 = Q[i]
+    name = "square1_" + str(q0[0]) + "_" + str(q0[1]) + "_" + str(q0[2]) + ".txt"
+    file = open(name, 'w')
+
+    # калибровка координат
+    q0 = [saturate(q0[0], -180, 180), saturate(q0[1], -70, 40), saturate(q0[2], -120, 100)]
+    q = [5 * q0[0], -5 * q0[1], -5/3 * q0[2]]
+
+    timeStart = time.time()
+    last_t = time.time()
+    sum = 0
+    last_e = 0
+
+    name = str(q0[0]) + "_" + str(q0[1]) + "_" + str(q0[2]) + ".txt"
+    file = open(name, 'w')
+
+    for j in range(3):
+        while abs(q[j] - motors_set[j].position) > inaccuracy:
+            e = q[j] - motors_set[j].position
+            dt = time.time() - last_t
+            U = k_p[j] * e + k_d[j] * (e - last_e) / dt + k_i[j] * sum * dt
+            U = U/U_max*100
+            motors_set[j].run_direct(duty_cycle_sp=saturate(U, -100, 100))
+            file.write(str(motorA.position) + '\t' + str(motorB.position) + '\t' + str(motorC.position) + '\t' + str(
+                saturate(U, -100, 100)) + '\t' + str(k_p[j] * e) + '\t' + str(k_d[j] * (e - last_e) / dt) + '\t' +
+                       str(k_i[j] * sum * dt) + '\n')
+            sum += e
+            last_e = e
+            last_t = time.time()
+        sum = 0
+        last_e = 0
+        last_t = time.time()
+        motors_set[j].run_direct(duty_cycle_sp=0)
+
+
+file.close()
