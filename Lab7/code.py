f61f5be551449062ad1f9b61cdd7567237088266
Anastasia Vyugina <75266068+avyuga@users.noreply.github.com>
2021-05-25 09:15:44 +0300

    2.1

--- Lab7/code.py
+++ Lab7/code.py
@@ -13,16 +13,17 @@ sound = Sound()
 sound.set_volume(100)
 sound.beep()
 
-# РїРµСЂРІР°СЏ РєРѕРѕСЂРґРёРЅР°С‚Р° - motorA
-# РІС‚РѕСЂР°СЏ РєРѕРѕСЂРґРёРЅР°С‚Р° - motorB
-# С‚СЂРµС‚СЊСЏ РєРѕРѕСЂРґРёРЅР°С‚Р° - motorC
+# первая координата - motorA
+# вторая координата - motorB
+# третья координата - motorC
 q0 = [90, 45, 45]
-q = [5 * q0[0], 5 * q0[1], 5/3 * q0[2]]
+q = [5 * q0[0], -5 * q0[1], -5/3 * q0[2]]
 
-# Р·РЅР°С‡РµРЅРёРµ РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРІ РІ РіСЂР°РґСѓСЃРЅС‹С… РјРµСЂР°С…
-k_p = [0.6, 0.3, 0.5]
-k_i = [0.00001, 0.00001, 0.00001]
-k_d = [0.9, 0.1, 0.1]
+# значение коэффициентов в градусных мерах
+k_p = [0.3, 0.3, 0.1]
+k_i = [0.25/60, 0.25/60, 0]
+k_d = [1/60, 1/60, 0]
+
 
 motorA = LargeMotor('outA')
 motorB = LargeMotor('outB')
@@ -36,27 +37,31 @@ timeStart = time.time()
 last_t = time.time()
 sum = 0
 last_e = 0
-inaccuracy = 10  # РїРѕРіСЂРµС€РЅРѕСЃС‚СЊ РІ РіСЂР°РґСѓСЃР°С…
+inaccuracy = 5  # погрешность в градусах
+U_max = 6.97
 
 name = str(q0[0]) + "_" + str(q0[1]) + "_" + str(q0[2]) + ".txt"
 file = open(name, 'w')
 
 motors_set = [motorA, motorB, motorC]
-while time.time() - timeStart < 20:
-    for i in range(3):
-        while abs(motors_set[i].position + inaccuracy) < q[i]:
-            e = q[i] - motors_set[i].position
-            dt = time.time() - last_t
-            U = k_p[i] * e + k_d[i] * (e - last_e) / dt + k_i[i] * sum * dt
-            motors_set[i].run_direct(duty_cycle_sp=saturate(U, -100, 100))
-            file.write(str(motorA.position) + '\t' + str(motorB.position) + '\t' + str(motorC.position) + '\t' + str(
-                saturate(U, -100, 100)) + '\n')
-            sum += e
-            last_e = e
-            last_t = time.time()
-        sum = 0
-        last_e = 0
+
+for i in range(3):
+    while abs(q[i] - motors_set[i].position) > inaccuracy:
+        e = q[i] - motors_set[i].position
+        dt = time.time() - last_t
+        U = k_p[i] * e + k_d[i] * (e - last_e) / dt + k_i[i] * sum * dt
+        U = U/U_max*100
+        motors_set[i].run_direct(duty_cycle_sp=saturate(U, -100, 100))
+        file.write(str(motorA.position) + '\t' + str(motorB.position) + '\t' + str(motorC.position) + '\t' + str(
+            saturate(U, -100, 100)) + '\t' + str(k_p[i] * e) + '\t' + str(k_d[i] * (e - last_e) / dt) + '\t' +
+                   str(k_i[i] * sum * dt) + '\n')
+        sum += e
+        last_e = e
         last_t = time.time()
+    sum = 0
+    last_e = 0
+    last_t = time.time()
+    motors_set[i].run_direct(duty_cycle_sp=0)
 
 
 file.close()
